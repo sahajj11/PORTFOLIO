cb5e147d9c19d7819bc949c7c7d59049a0dbd714
Sahaj Rajput <sahajrajput1112@gmail.com>
2024-10-28 10:39:07 +0530

    portfolio updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import myImage from "./pic 3.png";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+ 
 
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+
+
   return (
     <>
-      <div className="navbar d-flex justify-content-center fs-4 gap-4 hcc">
-        <Link to="/" style={{ textDecoration: 'none',color:'#333333' }}>Home</Link>
-        <Link to="/about" style={{ textDecoration: 'none',color:'#333333' }}>About</Link>
-        <Link to="/projects" style={{ textDecoration: 'none',color:'#333333' }}>Projects</Link>
-        <Link to="/resume" style={{ textDecoration: 'none',color:'#333333' }}>Resume</Link>
+       <div className="navbar d-flex justify-content-center hcc">
+       
+        <div className="nav-links-container fs-5 gap-5 d-flex">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/projects" className="nav-link">Projects</Link>
+          <Link to="/resume" className="nav-link">Resume</Link>
+        </div>
       </div>
 
-      <div className="d-flex mt-5 justify-content-center vh-100">
+      <div className="d-flex mt-5 flex-column align-items-center vh-100">
         <div className="text-center">
           <p className="display-1 hc">Hello!</p>
           <p className="display-1 hc">Sahaj Rajput Here</p>
-          <p className="hcc fs-3 mt-3">Full Stack Web Developer </p>
-
-          {/* <label className="theme-switch">
-          <input
-            type="checkbox"
-            checked={isDarkMode}
-            onChange={toggleTheme}
-          />
-          <span className="slider round"></span>
-        </label> */}
+          <p className="hcc fs-3 mt-3">
+            <span className="typing-text">Full Stack Web Developer</span>
+          </p>
 
           <div>
             <img
@@ -41,8 +36,8 @@ const Home = () => {
             />
           </div>
 
-          <div className="d-flex flex-column align-items-start ms-5 mt-5">
-            <div className="d-flex justify-content-center align-items-center mb-3">
+          <div className="footer-section mt-4">
+            <div className="mb-3">
               <a
                 href="https://www.linkedin.com/in/sahaj-rajput"
                 className="d-flex align-items-center text-decoration-none"
@@ -57,7 +52,7 @@ const Home = () => {
               </a>
             </div>
 
-            <div className="d-flex flex-coloumn align-items-start mb-3">
+            <div className="mb-3">
               <a
                 href="https://www.instagram.com/developerr_here/"
                 className="d-flex align-items-center text-decoration-none"
@@ -72,7 +67,7 @@ const Home = () => {
               </a>
             </div>
 
-            <div className="d-flex justify-content-center align-items-center mb-3">
+            <div className="mb-3">
               <a
                 href="https://github.com/sahajj11"
                 className="d-flex align-items-center text-decoration-none"
@@ -83,12 +78,11 @@ const Home = () => {
                   src="https://img.icons8.com/glyph-neue/64/github.png"
                   alt="instagram-new"
                 />
-                <span className="ms-2">
-                sahajj11</span>
+                <span className="ms-2">sahajj11</span>
               </a>
             </div>
 
-            <div className="d-flex justify-content-center align-items-center mb-3">
+            <div className="mb-3">
               <a
                 href="mailto:sahajrajput1112@gmail.com"
                 className="d-flex align-items-center text-decoration-none"
